5e1b3eea522ccb162eb0000ceb29e8d16979d629
Hasqer <ozanayrikan@gmail.com>
2022-05-13 21:30:25 +0300

    test login backend

--- Backend/Server.js
+++ Backend/Server.js
@@ -2,13 +2,11 @@ var express = require("express");
 var app = express();
 const path = require('path');
 var bodyParser = require("body-parser");
-<<<<<<< HEAD
-=======
+
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
->>>>>>> 4738f5088f8756bd2ad174dc0b97103a6543c024
 //var fs = require("fs");
 
 app.use(bodyParser.urlencoded({extended:false}));
@@ -16,7 +14,6 @@ app.use(bodyParser.json());
 app.use("/public", express.static("./../Frontend/public"));
 app.listen(process.env.PORT || 80);
 
-<<<<<<< HEAD
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/../Frontend/index.html'));
 });
@@ -26,11 +23,3 @@ app.post('/home', (req, res) => {
             res.sendFile(path.join(__dirname + '/../Frontend/homepage.html'));
         }
 });
-=======
-app.use("/public", express.static("./../Frontend/public"))
-
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/../Frontend/index.html'));
-});
->>>>>>> 4738f5088f8756bd2ad174dc0b97103a6543c024
