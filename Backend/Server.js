4738f5088f8756bd2ad174dc0b97103a6543c024
Sarusbr <batuilter2002@gmail.com>
2022-05-13 21:08:49 +0300

    edit

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,16 +1,17 @@
 var express = require("express");
 var app = express();
 const path = require('path');
+var bodyParser = require("body-parser");
 
-//var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
 //var fs = require("fs");
 app.listen(process.env.PORT || 80);
 
-app.use("/public", express.static("./public"))
+app.use("/public", express.static("./../Frontend/public"))
 
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/../Frontend/index.html'));
 });
-
-
