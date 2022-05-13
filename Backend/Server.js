c94bf2933b0d5c71389ee8a1ecd6cc0a8f01e56b
Hasqer <ozanayrikan@gmail.com>
2022-05-13 20:30:41 +0300

    frontend index publish

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,1 +1,16 @@
 var express = require("express");
+var app = express();
+const path = require('path');
+
+//var bodyParser = require("body-parser");
+//var fs = require("fs");
+app.listen(process.env.PORT || 80);
+
+app.use("/public", express.static("./public"))
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/../Frontend/index.html'));
+});
+
+
