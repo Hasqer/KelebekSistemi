98a22b8b6793a5611368b074f10c89cb0f9a3d12
Hasqer <ozanayrikan@gmail.com>
2022-05-13 21:29:21 +0300

    test login backend

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,16 +1,20 @@
 var express = require("express");
 var app = express();
 const path = require('path');
-
-//var bodyParser = require("body-parser");
+var bodyParser = require("body-parser");
 //var fs = require("fs");
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use("/public", express.static("./../Frontend/public"));
 app.listen(process.env.PORT || 80);
-
-app.use("/public", express.static("./public"))
-
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/../Frontend/index.html'));
 });
 
-
+app.post('/home', (req, res) => {
+        if(req.body.email == "test" && req.body.password == "test"){
+            res.sendFile(path.join(__dirname + '/../Frontend/homepage.html'));
+        }
+});
