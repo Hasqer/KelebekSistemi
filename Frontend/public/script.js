e809f4dc18d9a70aeaa6a8f7608954afb21da82a
Sarusbr <batuilter2002@gmail.com>
2022-05-14 22:32:45 +0300

    test

--- Frontend/public/script.js
+++ Frontend/public/script.js
@@ -1,22 +1,35 @@
 var app = new Vue({
-    el:".loginBox",
-    data:{
-        message:"test1"
+    el: ".loginBox",
+    data: {
+        UserInputEmail: "",
+        UserInputpassword: ""
     },
-    methods:{
-        button(){
+    methods: {
+        CheckUser() {
             alert("methoda girdim");
-            var mydata ={
-                email:"test@gmail.com",
-                password:"test"
+
+            var mydata = {
+                email: this.UserInputEmail,
+                password: ""
             }
-            fetch("/checkUser",{
-                method:"post",
-                body:JSON.stringify(mydata),
-                headers:{"Content-type":"application/json"}
+            this.ConvertHash(this.UserInputpassword).then((hesh) => mydata.password = hesh)
+            fetch("/checkUser", {
+                method: "post",
+                body: JSON.stringify(mydata),
+                headers: { "Content-type": "application/json" }
             })
-            .then(response => response.json())
-            .then(json => alert(json.check))
+                .then(response => response.json())
+                .then(json => alert(json.check))
+        },
+        ConvertHash(string) {
+            const utf8 = new TextEncoder().encode(string);
+            return crypto.subtle.digest('SHA-256', utf8).then((hashBuffer) => {
+                const hashArray = Array.from(new Uint8Array(hashBuffer));
+                const hashHex = hashArray
+                    .map((bytes) => bytes.toString(16).padStart(2, '0'))
+                    .join('');
+                return hashHex;
+            });
         }
     }
 })
