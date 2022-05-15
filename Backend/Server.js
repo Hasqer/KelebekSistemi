9a620292e2d41c210bfadef232d5134092fc6a97
Sarusbr <batuilter2002@gmail.com>
2022-05-15 17:34:10 +0300

    test

--- Backend/Server.js
+++ Backend/Server.js
@@ -4,9 +4,10 @@ var app = express();
 const path = require('path');
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var fs = require("fs");
 
 //Default settings
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/public", express.static("./../Frontend/public"));
 app.listen(process.env.PORT || 80);
@@ -49,18 +50,62 @@ app.post('/getUser', (req, res) => {
     res.send({ userInfo: "False" });
   }
 });
-
-var check = checkUser('test@gmail.com','test');
-check.then(function(result) {
-    console.log(result);
+/*
+var check = checkUser('test@gmail.com', 'test');
+check.then(function (result) {
+  console.log(result);
 });
+*/
+
+console.log(checkUser("test@gmail.com","test"));
+
+function checkUser(emailInfo, passwordInfo) {
+  var status = '11111';
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function (status) {
+    console.log("Connection Successful!");
+    // define Schema
+    var customersSchema = mongoose.Schema({
+      name: String,
+      surname: String,
+      email: String,
+      password: String,
+      created_time: Date
+    });
+    // compile schema to model
+    var customer = mongoose.model('Customer', customersSchema, 'customers');
+
+    //Check
+    var customerInfo = customer.findOne({ email: emailInfo, password: passwordInfo });
+    customerInfo.exec(()=>{
+      
+    });
+    customerInfo.then(result => {
+        console.log(result);
+    });
+    /*
+    customerInfo.then(result => {
+      if (result) {
+        status = 'False';
+        console.log("girdi");
+        return status;
+      }
+      else{
+        status = 'False';
+        return status;
+      }
+    });
+    */
+  })
+  console.log("bir daha girdi");
+};
 
 
+function createUser(nameInfo, surnameInfo, emailInfo, passwordInfo) {
 
-async function checkUser(emailInfo,passwordInfo){
-var status = '11111';
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function(status) {
+  db.on('error', console.error.bind(console, 'connection error:'));
+
+  db.once('open', function () {
     console.log("Connection Successful!");
 
     // define Schema
@@ -69,55 +114,19 @@ db.once('open', function(status) {
       surname: String,
       email: String,
       password: String,
-      created_time:Date
+      created_time: Date
     });
 
     // compile schema to model
     var customer = mongoose.model('Customer', customersSchema, 'customers');
 
-    //Check
-    var customerInfo = customer.findOne({ email: emailInfo, password:passwordInfo }).exec();
-    customerInfo.then(function(result) {
-        if(result){
-            status = 'False';
-            console.log("girdi");
-        }
-        else
-            status = 'False';
-     });
-})
-console.log("bir daha girdi");
-
-return status;
-};
-
-
-function createUser(nameInfo,surnameInfo,emailInfo,passwordInfo){
-
-    db.on('error', console.error.bind(console, 'connection error:'));
- 
-    db.once('open', function() {
-        console.log("Connection Successful!");
-         
-        // define Schema
-        var customersSchema = mongoose.Schema({
-          name: String,
-          surname: String,
-          email: String,
-          password: String,
-          created_time:Date
-        });    
-    
-        // compile schema to model
-        var customer = mongoose.model('Customer', customersSchema, 'customers');
-        
-        // new a document instance
-        var customer1 = new customer({ name: nameInfo,surname : surnameInfo,email: emailInfo, password: passwordInfo,created_time: Date.now()});
-        // save model to database
-        customer1.save(function (err, customer) {
-          if (err) return console.error(err);
-          console.log(customer.name + " saved to customer collection.");
-        });
-    })
+    // new a document instance
+    var customer1 = new customer({ name: nameInfo, surname: surnameInfo, email: emailInfo, password: passwordInfo, created_time: Date.now() });
+    // save model to database
+    customer1.save(function (err, customer) {
+      if (err) return console.error(err);
+      console.log(customer.name + " saved to customer collection.");
+    });
+  })
 
 };
