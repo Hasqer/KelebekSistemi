8a50d60980323e2947a1a002057e39500ab74ffd
Hasqer <ozanayrikan@gmail.com>
2022-05-15 20:36:36 +0300

    signIn backend coding

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,23 +1,26 @@
 //Variables
-var express = require("express");
+var express = require('express');
 var app = express();
 const path = require('path');
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-var fs = require("fs");
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var fs = require('fs');
 
-//Default settings
+//Main settings
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use("/public", express.static("./../Frontend/public"));
+app.use('/public', express.static('./../Frontend/public'));
 app.listen(process.env.PORT || 80);
+
+//Database settings
 main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect('mongodb+srv://admin:Password12@cluster0.uqoht.mongodb.net/KelebekSistemi?retryWrites=true&w=majority');
 }
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+
 
 
 
@@ -31,39 +34,46 @@ app.get('/home', (req, res) => {
   res.sendFile(path.join(__dirname + '/../Frontend/homepage.html'));
 });
 
-app.post('/checkUser', (req, res) => {
-  if (req.body.email == "test@gmail.com" && req.body.password == "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08") {
-    res.send({ check: "true" });
-    console.log(req.body);
-  }
-  else {
-    res.send({ check: "false" });
-    console.log(req.body);
-  }
+app.post('/checkCustomer', (req, res) => {
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  checkCustomer(email,password,function(result) {
+    if(result == 'true'){
+      res.send({ check: 'true' });
+    }
+    else if(result == 'false'){
+      res.send({ check: 'false' });
+    }
+  });
 });
 
-app.post('/getUser', (req, res) => {
-  if (req.body.email == "test@gmail.com" && req.body.password == "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08") {
-    res.send({ userInfo: "Abdurrahman Rasim" });
-  }
-  else {
-    res.send({ userInfo: "False" });
-  }
-});
-/*
-var check = checkUser('test@gmail.com', 'test');
-check.then(function (result) {
-  console.log(result);
-});
-*/
+app.post('/getCustomer', (req, res) => {
 
-console.log(checkUser("test@gmail.com","9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"));
+  const email = req.body.email;
+  const password = req.body.password;
+  
+  checkCustomer(email,password,function(result) {
+    if(result == 'true'){
+      getCustomer(email,password,function(result) {
+        res.send(result);
+      });
+    }
+    else if(result == 'false'){
+      res.send({ check: 'false' });
+    }
+  });
+});
 
-function checkUser(emailInfo, passwordInfo) {
-  var status = '11111';
-  db.on('error', console.error.bind(console, 'connection error:'));
+
+
+
+//Functions
+
+function checkCustomer(emailInfo, passwordInfo, callback) {
   db.once('open', function (status) {
-    console.log("Connection Successful!");
+    console.log('Connection Successful!');
     // define Schema
     var customersSchema = mongoose.Schema({
       name: String,
@@ -75,38 +85,62 @@ function checkUser(emailInfo, passwordInfo) {
     // compile schema to model
     var customer = mongoose.model('Customer', customersSchema, 'customers');
 
-    //Check
-    var customerInfo = customer.findOne({ email: emailInfo, password: passwordInfo });
-    customerInfo.exec(()=>{
-      
-    });/*
-    customerInfo.then(result => {
-        console.log(result);
-    });*/
-    /*
-    customerInfo.then(result => {
-      if (result) {
-        status = 'False';
-        console.log("girdi");
-        return status;
+    //Check from database
+    var customerInfo = customer.findOne({ email: emailInfo, password: passwordInfo }, function (err, data) {
+      if(data)
+        return callback('true');
+      else
+        return callback('false');
+        /*
+          Example CheckCustomer Code Block
+          checkCustomer('test@gmail.com','test',function(response) {
+            console.log(response);
+          });
+        */  
+    });
+  })};
+
+
+function getCustomer(emailInfo, passwordInfo,callback)
+{
+  console.log('girid');
+  db.once('open', function (status) {
+    console.log('Connection Successful!');
+    // define Schema
+    var customersSchema = mongoose.Schema({
+      name: String,
+      surname: String,
+      email: String,
+      password: String,
+      created_time: Date
+    });
+
+    // compile schema to model
+    var customer = mongoose.model('Customer', customersSchema, 'customers');
+    //Check from database
+    var customerInfo = customer.findOne({ email: emailInfo, password: passwordInfo }, function (err, data) {
+      if(data){
+        //return callback(data.name,data.surname,created_time);
+        var getCustomerJSON = {name: data.name,surname:data.surname,created_time:data.created_time};
+        return callback(getCustomerJSON);
       }
       else{
-        status = 'False';
-        return status;
-      }
-    });
-    */
-  })
-  console.log("bir daha girdi");
-};
+        return callback('false');
+        /*
+        getCustomer('test@gmail.com','test',function(result) {
+          res.send(result);
+        });
+        */
+      }});
+  })};
 
 
-function createUser(nameInfo, surnameInfo, emailInfo, passwordInfo) {
+function createCustomer(nameInfo, surnameInfo, emailInfo, passwordInfo) {
 
   db.on('error', console.error.bind(console, 'connection error:'));
 
   db.once('open', function () {
-    console.log("Connection Successful!");
+    console.log('Connection Successful!');
 
     // define Schema
     var customersSchema = mongoose.Schema({
@@ -125,8 +159,7 @@ function createUser(nameInfo, surnameInfo, emailInfo, passwordInfo) {
     // save model to database
     customer1.save(function (err, customer) {
       if (err) return console.error(err);
-      console.log(customer.name + " saved to customer collection.");
+      console.log(customer.name + ' saved to customer collection.');
     });
   })
-
 };
