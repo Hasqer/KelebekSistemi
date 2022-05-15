a536ce8c000b894be4687ede2986d25054b4872e
Hasqer <ozanayrikan@gmail.com>
2022-05-16 01:16:36 +0300

    SignIn SignUp backend codes

--- Backend/Server.js
+++ Backend/Server.js
@@ -5,16 +5,33 @@ const path = require('path');
 var bodyParser = require('body-parser');
 var mongoDb = require('mongodb').MongoClient;
 const randomHash = require('random-hash');
+const nodeMailer = require('nodemailer');
+const domain = "localhost";
 
 //Main settings
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 app.use('/public', express.static('./../Frontend/public'));
 app.listen(process.env.PORT || 80);
 
 //Database settings
-// Connection URL
 const url = 'mongodb+srv://admin:Password12@cluster0.uqoht.mongodb.net/test';
+
+//Mail settings
+let mailTransporter = nodeMailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'kelebeksistem@gmail.com',
+    pass: 'Butterfly123'
+  }
+});
+
+
+
+
+
 
 
 
@@ -34,12 +51,15 @@ app.post('/checkCustomer', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  checkCustomer(email,password,function(result) {
-    if(result == 'true'){
-      res.send({ check: 'true' });
-    }
-    else if(result == 'false'){
-      res.send({ check: 'false' });
+  checkCustomer(email, password, function (result) {
+    if (result == 'true') {
+      res.send({
+        check: 'true'
+      });
+    } else if (result == 'false') {
+      res.send({
+        check: 'false'
+      });
     }
   });
 });
@@ -48,130 +68,163 @@ app.post('/getCustomer', (req, res) => {
 
   const email = req.body.email;
   const password = req.body.password;
-  
-  checkCustomer(email,password,function(result) {
-    if(result == 'true'){
-      getCustomer(email,password,function(result) {
+
+  checkCustomer(email, password, function (result) {
+    if (result == 'true') {
+      getCustomer(email, password, function (result) {
         res.send(result);
       });
-    }
-    else if(result == 'false'){
-      res.send({ check: 'false' });
+    } else if (result == 'false') {
+      res.send({
+        check: 'false'
+      });
     }
   });
 });
 
 app.post('/createCustomer', (req, res) => {
 
-  if(req.body.nameInfo && req.body.surnameInfo && req.body.emailInfo && req.body.passwordInfo){
-    const randomVerificationCode = randomHash.generateHash({ length: 15 });
-    createCustomer(req.body.nameInfo,req.body.surnameInfo,req.body.emailInfo,randomVerificationCode,req.body.passwordInfo);
-    res.send({ check: 'true' }); //Successful SignUp
-    //CODE MAİL WILL SEND HERE
-  }
-  else{
-    res.send({ check: 'false' }); //Failed SignUp
+  if (req.body.nameInfo && req.body.surnameInfo && req.body.emailInfo && req.body.passwordInfo) {
+    const randomVerificationCode = randomHash.generateHash({
+      length: 15
+    });
+    createCustomer(req.body.nameInfo, req.body.surnameInfo, req.body.emailInfo, randomVerificationCode, req.body.passwordInfo);
+    res.send({
+      check: 'true'
+    }); //Successful SignUp
+    verificationMailSend("http://" + domain + "/emailVerification/" + randomVerificationCode);
+
+  } else {
+    res.send({
+      check: 'false'
+    }); //Failed SignUp
   }
 });
 
-app.get('/emailVerification/:verificationCodeCheck', (req, res) => {
-  checkCustomer('test@gmail.com','test');
-  
-  //emailVerificationCheck("Verified");
-  //res.end();
+app.get('/emailVerification/:emailVerificationCode', (req, res) => {
 
-  //res.redirect('/');
+  const emailVerificationCode = req.params.emailVerificationCode;
+  emailVerificationCheck(emailVerificationCode, function (result) {
+    if (result == false)
+      res.send({
+        check: "false"
+      });
+    else if (result == true)
+      res.send({
+        check: "true"
+      });
+  });
 });
+
+
 
 
 
 
 //Functions
 function checkCustomer(emailInfo, passwordInfo, callback) {
-    mongoDb.connect(url,function(err,client){
+  mongoDb.connect(url, function (err, client) {
     const db = client.db("KelebekSistemi");
-    db.collection("customers").findOne({ email: emailInfo, password: passwordInfo },(err,data)=>{
-    if(data)
-       callback('true');
-    else
-       callback('false');
-  });
-  })};
-
-
-function getCustomer(emailInfo, passwordInfo,callback)
-{
-
-  mongoDb.connect(url,function(err,client){
-    const db = client.db("KelebekSistemi");
-    db.collection("customers").findOne({ email: emailInfo, password: passwordInfo },(err,data)=>{
-    if(data){
-        var getCustomerJSON = {name: data.name,surname:data.surname,created_time:data.created_time};
-        return callback(getCustomerJSON);
-    }
-    else{
-        callback('false');
-    }
-  });
-  })};
-
-
-
-function createCustomer(nameInfo, surnameInfo, emailInfo,emailVerificationCodeInfo, passwordInfo) {
-
-
-    mongoDb.connect(url,function(err,client){
-    const db = client.db("KelebekSistemi");
-    var customerObj = {
-      name: nameInfo,
-      surname: surnameInfo,
+    db.collection("customers").findOne({
       email: emailInfo,
-      emailVerificationBool:false,
-      emailVerificationCode:emailVerificationCodeInfo,
-      password: passwordInfo,
-      paidCustomer:false,
-      licenseDeadline:"",
-      created_time: Date.now()};
-    db.collections("customers").insertOne(customerObj,(err,data)=>{
-    if(data){
-        
-        return callback(getCustomerJSON);
-    }
-    else{
+      password: passwordInfo
+    }, (err, data) => {
+
+      if (data) {
+        callback('true');
+        client.close();
+      } else {
         callback('false');
-    }
-  });
-  })};
-
-
-
-function emailVerificationCheck(emailVerificationCodeCheck) {
-  
-
-
-
-  mongoDb.connect(url,function(err,client){
-    const db = client.db("KelebekSistemi");
-    var verifiedEmail 
-    db.collection("customers").updateOne({ email: emailInfo, password: passwordInfo },(err,data)=>{
-    if(data){
-        var getCustomerJSON = {name: data.name,surname:data.surname,created_time:data.created_time};
-        return callback(getCustomerJSON);
-    }
-    else{
-        callback('false');
-    }
-  });
-})};
-
-
-
-
-/*
-
-
-    var customerInfo = customer.findOneAndUpdate({ emailVerificationCode:emailVerificationCodeCheck },{$set:{emailVerificationBool:false,emailVerificationCode:"Verified"}}, function (err, data) {
-      console.log("Onaylandı");
+      }
     });
-  })};
- */
+  })
+};
+
+function getCustomer(emailInfo, passwordInfo, callback) {
+
+  mongoDb.connect(url, function (err, client) {
+    const db = client.db("KelebekSistemi");
+    db.collection("customers").findOne({
+      email: emailInfo,
+      password: passwordInfo
+    }, (err, data) => {
+      if (data) {
+        var getCustomerJSON = {
+          name: data.name,
+          surname: data.surname,
+          created_time: data.createdTime
+        };
+        return callback(getCustomerJSON);
+        client.close();
+      } else {
+        callback('false');
+        client.close();
+      }
+    });
+  })
+};
+
+function createCustomer(nameInfo, surnameInfo, emailInfo, emailVerificationCodeInfo, passwordInfo) {
+
+  var customerObj = {
+    name: nameInfo,
+    surname: surnameInfo,
+    email: emailInfo,
+    emailVerificationBool: false,
+    emailVerificationCode: emailVerificationCodeInfo,
+    password: passwordInfo,
+    paidCustomer: false,
+    licenseDeadline: "",
+    createdTime: Date.now()
+  };
+
+  mongoDb.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("KelebekSistemi");
+
+    dbo.collection("customers").insertOne(customerObj, (error, islem) => {
+
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  });
+};
+
+function emailVerificationCheck(emailVerificationCodeCheck, callback) {
+  mongoDb.connect(url, function (err, client) {
+    const db = client.db("KelebekSistemi");
+    db.collection("customers").updateOne({
+      emailVerificationCode: emailVerificationCodeCheck
+    }, {
+      $set: {
+        emailVerificationBool: true,
+        emailVerificationCode: "Verified"
+      }
+    }, (err, data) => {
+      if (data.matchedCount == "1")
+        callback(true);
+      else if (data.matchedCount == "0")
+        callback(false);
+    });
+  })
+};
+
+function verificationMailSend(verificationLink) {
+
+  let mailDetails = {
+    from: 'Kelebek Sistemi <kelebeksistem@gmail.com>',
+    to: 'ozanayrikan@gmail.com',
+    subject: 'Kelebek Sistemi e-Posta adresinizi doğrulayın',
+    text: `kelebek sistemi e-posta adresinizi doğrulayın 
+    link = ` + verificationLink
+  };
+
+  mailTransporter.sendMail(mailDetails, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Email sent successfully');
+    }
+  });
+};
